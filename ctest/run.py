1778c81958942eb07d58a7e180477313c97f3a31
Teodoro Fields Collin <teocollin1995@gmail.com>
2019-02-22 15:38:53 -0600

    finished up piping

--- ctest/run.py
+++ ctest/run.py
@@ -1,8 +1,34 @@
 import ctypes as ct
 import firedrake_build as fb
-import structs as st
+import os
+import passing
+from firedrake import *
 
+#Firedrake stuff:
+mesh = UnitSquareMesh(5,5)
+space = FunctionSpace(mesh, "Lagrange", 4)
+f = Function(space)
+
+#Diderot stuff:
+#choose types...
+intTy = ct.c_int32
+floatTy = ct.c_float
+# build json
+jsonFile = "test.json"
+if os.path.exists(jsonFile):
+    pass
+else:
+    fb.spaceToJson(space, "test.json", refCellDefault="simplex")
+# build data
+femArgs = fb.passAll(f, intTy, floatTy)
+
+#program:
 programNameArg = "justTypes"
 nameSpaceArg = "justTypes"
-
 library = ct.CDLL("./" + programNameArg + ".so")
+program = passing.Library(library, nameSpace=nameSpaceArg)
+
+femArgsNamed = {"a": femArgs[0], "b": femArgs[1], "c": femArgs[2]}
+outputs = ["pos"]
+program.go(femArgsNamed, outputs)
+
