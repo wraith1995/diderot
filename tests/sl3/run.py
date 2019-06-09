7e1be1b039f26e66eb467dccc4f6af2c2f66b1da
Teodoro Fields Collin <teocollin1995@gmail.com>
2019-06-09 16:56:49 -0500

    run before changing to time max scheme

--- tests/sl3/run.py
+++ tests/sl3/run.py
@@ -5,6 +5,9 @@ sys.path.append('/home/teocollin/gitcode/curvedMesh')
 sys.path.append('s1')
 sys.path.append('s2')
 sys.path.append('s3')
+sys.path.append('s4')
+sys.path.append('s5')
+sys.path.append('s8')
 from firedrake import *
 import ctypes as ct
 import firedrake_build as fb
@@ -22,7 +25,10 @@ from render import render, compareError, compareErrorSingle, compareToBackSolveS
 from load import buildFiredrakeTetMesh
 from runGuided import runGuided
 from runGuidedErr import runGuidedErr
+from runGuidedRk import runGuidedRk
+from runGuidedErrRk import runGuidedErrRk
 from runTruth import runTruth
+from runTruthFaster import runTruthFaster
 #get the damn mesh
 meshDatas = buildFiredrakeTetMesh("meshfiles/gsc5.msh", 3, linearGmshBackup="lin_meshfiles/gsc5.msh")
 linMesh = meshDatas[-1]
@@ -49,43 +55,27 @@ f = interpolate(as_vector(Mx), space)
 
 
 (_, femArgs) = fb.passAll(f, intTy, floatTy, geometric=True)
-#points = [[-0.8, 0.1, 0.2]]
+points = [[-0.8, 0.1, 0.2]]
 #points = [[0.85, 0.0, 0.2]]
-points = [[-0.875, 0.1, 0.2]]
+#points = [[-0.875, 0.1, 0.2]]
 pointsNrrd = "points.nrrd"
 dataPoints = np.array(points, dtype="float64")
 kindString = "{0}-vector".format(dim)
 nu.writeSequence(dataPoints, pointsNrrd, dataKind=kindString)
 
-# dumb outright/fix validity issues...
-#loop over, calc errors and vtks
-#calc error via time step/time slowdown
-#invalid position creation...
-#runtime calc
-#PVO IS MORE INTERESTING BECAUSE ISO
-#other mesh.
-#perfoamnce.
-# runTruth("umm", pointsNrrd, femArgs, stepSize=0.5, stepMax=500)
-# render("umm", "umm", dim=dim)
-# runTruth("umm1", pointsNrrd, femArgs, stepSize=0.05, stepMax=500)
-# render("umm1", "umm1", dim=dim)
-# # exit(0)
 
 #add another mesh
-#add timing calculate
-#add error calculate
-#yay!
 #write this section
-#writhe methods + conclusion
 
 stepSizes = [0.5, 0.05, 0.005, 0.0005]
 stepsMaxes = [5000]
 #common to newtons
 timeStepses = [32]
-secondes = [0, 1]
+secondes = [0]
+innerRkSteps = [1, 2, 4, 8]
 timeEpses = [0.0000001]
 #specific to error:
-errorMaxes = [0.1, 0.01, 0.001, 0.0001, 0.0000001]
+errorMaxes = [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001]
 prefixNrrd = "nrrdResults/"
 prefixVtk = "vtkResults/"
 errFileDict = dict()
@@ -100,52 +90,80 @@ for stepSize in stepSizes:
         timeTrue = runTruth(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize,
                             stepMax=stepsMax)
         render(firstNrrd, prefixVtk + first + "_truth")
-        timingDict[(stepSize, stepsMax)] = (timeTrue[1], dict())
+        timeTrueFaster = runTruthFaster(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize,
+                                        stepMax=stepsMax)
+
+        timingDict[(stepSize, stepsMax)] = (timeTrue[1], timeTrueFaster[1], dict())
         errorDict[(stepSize, stepsMax)] = dict()
         for timeStep in timeStepses:
             for timeEps in timeEpses:
                 for second in secondes:
-                    guide = first + "_{0}_{1}_{2}_guide".format(str(timeStep), str(timeEps), str(second))
-                    guideNrrd = prefixNrrd + guide
-                    timeGuide = runGuided(guideNrrd, pointsNrrd, femArgs,
-                                          stepSize=stepSize, stepMax=stepsMax,
-                                          timeSteps=timeStep, timeEps=timeEps,
-                                          second=second)
-                    timingDict[(stepSize, stepsMax)][1][(timeStep, timeEps, second)] = (timeGuide[1], dict())
-                    errorDict[(stepSize, stepsMax)][(timeStep, timeEps, second)] = (compareErrorSingle(firstNrrd, guideNrrd), dict())
-                    render(guideNrrd, prefixVtk + guide)
-                    errorComps.append(guideNrrd)
-                    for maxError in errorMaxes:
-                        guideErr = guide + "_{0}_checked".format(str(maxError))
-                        guideErrNrrd = prefixNrrd + guideErr
-                        timeErrGuide = runGuidedErr(guideErrNrrd, pointsNrrd, femArgs,
-                                                    stepSize=stepSize, stepMax=stepsMax,
-                                                    timeSteps=timeStep, timeEps=timeEps,
-                                                    second=second, errorMax=maxError)
-                        timingDict[(stepSize, stepsMax)][1][(timeStep, timeEps, second)][1][maxError] = timeErrGuide[1]
-                        errorDict[(stepSize, stepsMax)][(timeStep, timeEps, second)][1][maxError] = compareErrorSingle(firstNrrd, guideErrNrrd)
-                        render(guideErrNrrd, prefixVtk + guideErr)
-                        errorComps.append(guideErrNrrd)
-        print(errorComps)
-        errFileDict[(stepSize, stepsMax)] = errorComps
+                    for rkStep in innerRkSteps:
+                        guide = first + "_{0}_{1}_{2}_{3}_guide".format(str(timeStep), str(timeEps), str(second), rkStep)
+                        guideNrrd = prefixNrrd + guide
+                        if rkStep == 1:
+                            timeGuide = runGuided(guideNrrd, pointsNrrd, femArgs,
+                                                  stepSize=stepSize, stepMax=stepsMax,
+                                                  timeSteps=timeStep, timeEps=timeEps,
+                                                  second=second)
+                            timeTrueComp = runTruth(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize, stepMax=timeGuide[0], save=False)
+                            timeTrueCompFaster = runTruthFaster(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize,
+                                                                stepMax=timeGuide[0], save=False)
+                        else:
+                            timeGuide = runGuidedRk(guideNrrd, pointsNrrd, femArgs,
+                                                  stepSize=stepSize, stepMax=stepsMax,
+                                                  timeSteps=timeStep, timeEps=timeEps,
+                                                  second=second, innerRkStep=rkStep)
+                            timeTrueComp = runTruth(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize, stepMax=timeGuide[0], save=False)
+                            timeTrueCompFaster = runTruthFaster(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize,
+                                                                   stepMax=timeGuide[0], save=False)
+                        timingDict[(stepSize, stepsMax)][2][(timeStep, timeEps, second, rkStep)] = ((timeGuide[1], timeTrueComp[1], timeTrueCompFaster[1]), dict())
+                        errorDict[(stepSize, stepsMax)][(timeStep, timeEps, second, rkStep)] = (compareErrorSingle(firstNrrd, guideNrrd), dict())
+                        render(guideNrrd, prefixVtk + guide)
+                        errorComps.append(guideNrrd)
+                        for maxError in errorMaxes:
+                            guideErr = guide + "_{0}_checked".format(str(maxError))
+                            guideErrNrrd = prefixNrrd + guideErr
+                            if rkStep == 1:
+                                timeErrGuide = runGuidedErr(guideErrNrrd, pointsNrrd, femArgs,
+                                                            stepSize=stepSize, stepMax=stepsMax,
+                                                            timeSteps=timeStep, timeEps=timeEps,
+                                                            second=second, errorMax=maxError)
+                                timeErrGuideTrueComp = runTruth(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize, stepMax=timeErrGuide[0], save=False)
+                                timeErrGuideTrueCompFaster = runTruthFaster(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize, stepMax=timeErrGuide[0], save=False)
+                            else:
+                                timeErrGuide = runGuidedErrRk(guideErrNrrd, pointsNrrd, femArgs,
+                                                              stepSize=stepSize, stepMax=stepsMax,
+                                                              timeSteps=timeStep, timeEps=timeEps,
+                                                              second=second, errorMax=maxError, innerRkStep=rkStep)
+                                timeErrGuideTrueComp = runTruth(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize, stepMax=timeErrGuide[0], save=False)
+                                timeErrGuideTrueCompFaster = runTruthFaster(firstNrrd, pointsNrrd, femArgs, stepSize=stepSize, stepMax=timeErrGuide[0], save=False)
+                                
+                            timingDict[(stepSize, stepsMax)][2][(timeStep, timeEps, second, rkStep)][1][maxError] = (timeErrGuide[1], timeErrGuideTrueComp[1], timeErrGuideTrueCompFaster[1])
+                            errorDict[(stepSize, stepsMax)][(timeStep, timeEps, second, rkStep)][1][maxError] = compareErrorSingle(firstNrrd, guideErrNrrd)
+                            render(guideErrNrrd, prefixVtk + guideErr)
+                            errorComps.append(guideErrNrrd)
+                            print(errorComps)
+                            errFileDict[(stepSize, stepsMax)] = errorComps
 
 def makeTiminingList(timings):
-    titles = ["stepSize", "stepsMax", "guideTimeStep", "guideTimeEps", "dervApproxType", "maxErrorParam", "time"]
+    titles = ["stepSize", "stepsMax", "guideTimeStep", "guideTimeEps", "dervApproxType", "innerRkSteps", "maxErrorParam", "time", "trueTime", "trueTimeFast", "trueTime/time", "trueTimeFast/time"]
     data = []
     for entry in timings.keys():
         firstTwo = entry
         print(firstTwo)
-        data.append([firstTwo[0], firstTwo[1], float("nan"), float("nan"), float("nan"), float("nan"), timings[entry][0]])
-        tempTimings = timings[entry][1]
+        data.append([firstTwo[0], firstTwo[1], float("nan"), float("nan"), float("nan"), float("nan"), float("nan"), timings[entry][0], float("nan"), float("nan"), float("nan")])
+        data.append([firstTwo[0], firstTwo[1], float("nan"), float("nan"), float("nan"), float("nan"), float("nan"), timings[entry][1], float("nan"), float("nan"), float("nan")])
+        tempTimings = timings[entry][2]
         for entryNext in tempTimings.keys():
-            data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], float("inf"), tempTimings[entryNext][0]])
+            data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], entryNext[3], float("inf"), tempTimings[entryNext][0][0], tempTimings[entryNext][0][1], tempTimings[entryNext][0][2], tempTimings[entryNext][0][1]/tempTimings[entryNext][0][0], tempTimings[entryNext][0][2]/tempTimings[entryNext][0][0]])
             tempTimingsPrime = tempTimings[entryNext][1]
             for entryFin in tempTimingsPrime.keys():
-                data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], entryFin, tempTimingsPrime[entryFin]])
+                data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], entryNext[3], entryFin, tempTimingsPrime[entryFin][0], tempTimingsPrime[entryFin][1], tempTimingsPrime[entryFin][2],  tempTimingsPrime[entryFin][1]/tempTimingsPrime[entryFin][0], tempTimingsPrime[entryFin][2]/tempTimingsPrime[entryFin][0]])
     return(titles, data)
 
 def makeErrorsList(timings):
-    titles = ["stepSize", "stepsMax", "guideTimeStep", "guideTimeEps", "dervApproxType", "maxErrorParam", "stepDiffs", "maxError", "averageError"]
+    titles = ["stepSize", "stepsMax", "guideTimeStep", "guideTimeEps", "dervApproxType", "innerRkSteps", "maxErrorParam", "stepDiffs", "maxError", "averageError"]
     data = []
     for entry in timings.keys():
         firstTwo = entry
@@ -153,11 +171,11 @@ def makeErrorsList(timings):
         tempTimings = timings[entry]
         for entryNext in tempTimings.keys():
             errors = [tempTimings[entryNext][0][0], tempTimings[entryNext][0][1], tempTimings[entryNext][0][3]]
-            data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], float("inf"), *errors])
+            data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], entryNext[3], float("inf"), *errors])
             tempTimingsPrime = tempTimings[entryNext][1]
             for entryFin in tempTimingsPrime.keys():
                 otherErrors = [tempTimingsPrime[entryFin][0], tempTimingsPrime[entryFin][1], tempTimingsPrime[entryFin][3]]
-                data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], entryFin, *otherErrors])
+                data.append([firstTwo[0], firstTwo[1], entryNext[0], entryNext[1], entryNext[2], entryNext[3], entryFin, *otherErrors])
     return(titles, data)
 
 def makeTimingsCsv(fileName, timings):
@@ -185,7 +203,7 @@ def makeCombined(fileName, timings, errors):
         writer.writerow(combinedTitles)
         for timing in dataTimings:
             if not(timing[2] == timing[2]):
-                writer.writerow(timing + [0.0, 0.0, 0.0])
+                writer.writerow(timing + [float("nan"), float("nan"), float("nan"), float("nan")])
                 continue
             if len(dataErrors) > 0:
                 topDataErr = dataErrors[0]
